9dec8d429277b52e9e9e4f0af8779bfe82b10657
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 12:15:40 +0300

    Added Animal and Comment models.

---
+++ PetShopProject/Models/Animal.cs
@@ -0,0 +1,54 @@
+﻿using AspNetCoreCourseProject.ValidationAttrs;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Models
+{
+
+    namespace AspNetCoreCourseProject.Models
+    {
+        public class Animal
+        {
+            public int AnimalId { get; set; }
+            [Required(ErrorMessage = "לא כתבת את שם החיה!")]
+            [StringLength(30, MinimumLength = 3, ErrorMessage = "שם החיה צריך להיות בין 3 ל30 תווים!")]
+            [Display(Name = "שם החיה")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "לא כתבת את גיל החיה!")]
+            [Range(0.1, 80, ErrorMessage = "גיל צריך להיות גדול מ0 וקטן מ80.")]
+            [Display(Name = "גיל החיה")]
+            public double Age { get; set; }
+            [Display(Name = "תמונה מייצגת")]
+            public string PictureName { get; set; }
+            [Required(ErrorMessage = "לא כתבת תיאור לחיה!")]
+            [MinLength(5, ErrorMessage = "התיאור חייב להכיל לכל הפחות 5 תווים!")]
+            [Display(Name = "תיאור בעל החי")]
+            public string Description { get; set; }
+            [Display(Name = "קטגוריה")]
+            public virtual Category Category { get; set; }
+            public virtual ICollection<Comment> Comments { get; set; }
+
+            #region CategoryId for form
+            [NotMapped]
+            [Required(AllowEmptyStrings = false, ErrorMessage = "לא בחרת קטגוריה!")]
+            [Display(Name = "קטגוריה")]
+            public int? FormCategoryId { get; set; }
+            #endregion
+
+
+            #region File UPload
+            [NotMapped]
+            [DataType(DataType.Upload)]
+            [Display(Name = "תמונה מייצגת")]
+            public IFormFile FormFile { get; set; }
+            #endregion
+
+        }
+    }
+
+}
---
+++ PetShopProject/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using PetShopProject.Models.AspNetCoreCourseProject.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetShopProject.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public virtual Animal Animal { get; set; }
+        [Required(ErrorMessage = "עליך לכתוב תגובה!")]
+        [Display(Name = "כתוב תגובתך")]
+        [DataType(DataType.MultilineText)]
+        [StringLength(300, MinimumLength = 3, ErrorMessage = "תגובה תהיה באורך 3 תווים עד 300")]
+        public string CommentText { get; set; }
+    }
+}
