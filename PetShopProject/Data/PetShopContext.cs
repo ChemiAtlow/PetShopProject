fc66e2dba307b9f688eaed5c5d4d0957b40807aa
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 12:35:49 +0300

    Add DbContext

---
+++ PetShopProject/Data/PetShopContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShopProject.Models;
+
+namespace PetShopProject.Data
+{
+    public class PetShopContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public PetShopContext(DbContextOptions<PetShopContext> options) : base(options) { }
+
+    }
+}
