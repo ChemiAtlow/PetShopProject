274939f86543650eb83a34e0fdafe4c92fccd2f3
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:58:38 +0300

    Add animapl page

---
+++ PetShopProject/Controllers/AnimalController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using PetShopProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopProject.Controllers
+{
+    public class AnimalController : Controller
+    {
+        private readonly IRepository repository;
+
+        public AnimalController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            try
+            {
+                return View(await repository.GetAnimalDetailsAsync(id));
+            }
+            catch (Exception e)
+            {
+                return View("Error", e);
+            }
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddComment(string commentText, int animalId)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await repository.AddCommentAsync(commentText.Trim(), animalId);
+                    return RedirectToAction("Index", new { id = animalId });
+                }
+                throw new ArgumentException("The fields are invalid!");
+            }
+            catch (Exception e)
+            {
+                return View("Error", e);
+            }
+        }
+    }
+}
