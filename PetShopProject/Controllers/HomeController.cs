4aae090a437fb3cd0e19ffa41057e290c7cd3baf
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:51:20 +0300

    Add home page

---
+++ PetShopProject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShopProject.Repositories;
+
+namespace PetShopProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IRepository repository;
+
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(repository.GetTwoMostCommentedAnimals());
+        }
+    }
+}
