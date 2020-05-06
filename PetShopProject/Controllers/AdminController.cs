93abae68679e81805cffd243670b664bce7c9074
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 14:02:07 +0300

    Add abillity to edit and add new animals

--- PetShopProject/Controllers/AdminController.cs
+++ PetShopProject/Controllers/AdminController.cs
@@ -49,7 +49,55 @@ namespace PetShopProject.Controllers
                 return View("Error", e);
             }
         }
-        
+        [HttpGet]
+        public async Task<IActionResult> EditAnimal(int id = 0)
+        {
+            ViewBag.Categories = repository.GetCategories();
+            Animal animal = new Animal();
+            if (id != 0)
+            {
+                animal = await repository.GetAnimalDetailsAsync(id);
+                animal.FormCategoryId = animal.Category.CategoryId;
+            }
+
+            return View(animal);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAnimal(Animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                if (animal.FormFile != null)
+                {
+                    if (animal.PictureName != null)
+                    {
+                        DeleteOldImage(animal.PictureName);
+                    }
+                    string imageName = await UploadImage(animal.FormFile);
+                    animal.PictureName = imageName;
+                }
+                await repository.AddOrEditAnimalAsync(animal);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Categories = repository.GetCategories();
+            return View(animal);
+        }
+
+        /// <summary>
+        /// Upload an image for a new or edited animal.
+        /// </summary>
+        /// <param name="formFile">File to be uploaded.</param>
+        /// <returns>Name of file to be saved in DB.</returns>
+        private async Task<string> UploadImage(IFormFile formFile)
+        {
+            string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+            string uniqueFileName = Guid.NewGuid().ToString() + "_" + formFile.FileName;
+            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+            using var fileStream = new FileStream(filePath, FileMode.Create);
+            await formFile.CopyToAsync(fileStream);
+            return uniqueFileName;
+        }
         /// <summary>
         /// Delete the old picture of animal when deleted or updated it's picture.
         /// </summary>
