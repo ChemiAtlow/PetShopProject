0c13300a648ca1d3ecaaabdfd2c29f182d2b3097
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:54:55 +0300

    Add a pagination ViewComponent

---
+++ PetShopProject/ViewComponents/PaginationViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using PetShopProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PetShopProject.ViewComponents
+{
+    public class PaginationViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(PaginationData pagination)
+        {
+            return await Task.FromResult(View(pagination));
+        }
+    }
+}
