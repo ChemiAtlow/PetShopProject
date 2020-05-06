5cfd615f76c7618e152c5eae8165d2f0ecd569e8
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:50:49 +0300

    Create a pet info ViewComponent

---
+++ PetShopProject/ViewComponents/PetInfoViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using PetShopProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PetShopProject.ViewComponents
+{
+    public class PetInfoViewComponent : ViewComponent
+    {
+        public enum PetInfoType { HomePage, Catalog, Admin };
+        public async Task<IViewComponentResult> InvokeAsync(Animal animal, PetInfoType infoType)
+        {
+            ViewBag.InfoType = infoType;
+            return await Task.FromResult(View(animal));
+        }
+
+    }
+}
