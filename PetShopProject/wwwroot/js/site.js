3ae284ada38685c32eba4c5b54283707b0b47462
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:28:58 +0300

    Add site JS file, and handle mobile navigation

---
+++ PetShopProject/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿/*Navigation*/
+const backdrop = $(".backdrop");
+const sideDrawer = $(".main-header__nav");
+const menuToggle = $("#side-menu-toggle");
+
+backdrop.on("click", function () {
+    $(this).hide();
+    sideDrawer.removeClass("open");
+});
+
+menuToggle.on("click", function () {
+    backdrop.show("fast");
+    sideDrawer.addClass("open");
+});
