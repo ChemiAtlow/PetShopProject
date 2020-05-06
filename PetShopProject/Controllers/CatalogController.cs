500c1d15610c6c34e53e96527af95fa2bec90f34
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:55:43 +0300

    Add catalog page

---
+++ PetShopProject/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using PetShopProject.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PetShopProject.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private readonly IRepository repository;
+
+        public CatalogController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IActionResult Index(int id = 0, int page = 1)
+        {
+            try
+            {
+                var (pagination, animals) = repository.GetAnimals(id, page);
+                ViewBag.CurrentCategoryId = id;
+                ViewBag.Categories = repository.GetCategories();
+                ViewBag.Pagination = pagination;
+                return View(animals);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                return View("Error", e);
+            }
+        }
+    }
+}
