9ba04d48a67321a0cd0d23afe212c13ddb9d720b
Chemi Atlow <chemiatlow@gmail.com>
2020-05-06 13:11:17 +0300

    Added a ValidationAttribute to test if category passed is a valid category

---
+++ PetShopProject/Attributes/ValidCategoryAttribute.cs
@@ -0,0 +1,37 @@
+﻿using PetShopProject.Models;
+using PetShopProject.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace PetShopProject.Attributes
+{
+    public class ValidCategoryAttribute : ValidationAttribute
+    {
+        private const string DefaultCategoryNotFoundMessage = "קטגוריה זו לא קיימת!";
+        private const string DefaultErrorMessage = "יש להזין קטגוריה!";
+
+        public string CategoryNotFoundMessage { get; set; }
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var configService = validationContext.GetService<IRepository>();
+            if (value is int categoryId)
+            {
+                Task<Category> t = CheckCategory(categoryId, configService);
+                t.Wait();
+                if (validationContext.ObjectInstance is Animal animal)
+                    animal.Category = t.Result;
+                return t.Result != null ? ValidationResult.Success : new ValidationResult(CategoryNotFoundMessage ?? DefaultCategoryNotFoundMessage);
+            }
+            else
+            {
+                return new ValidationResult(ErrorMessage ?? DefaultErrorMessage);
+            }
+        }
+
+        private async Task<Category> CheckCategory(int categoryId, IRepository repository)
+        {
+            return await repository.GetCategoryAsync(categoryId);
+        }
+    }
+}
